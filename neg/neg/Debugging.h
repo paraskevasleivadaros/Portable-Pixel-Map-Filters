d14ebdb76e19603f31b538f82c34440ecbe869ba
paraskevasleivadaros <paraskevasleivadaros@gmail.com>
2018-12-19 03:49:28 +0200

    deleted unwanted files

--- neg/neg/Debugging.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#ifdef _DEBUG
-	#define BoolToString(b) ((b) ? "true" : "false")
-	#define PrintValInfo(x) {std::cout << "Variable '" << #x << "': " << &x << "[" << BoolToString(x) << "]\n\n";}
-	#define PrintPtrInfo(x) {std::cout << "Pointer to object '" << #x << "': " << &x << "[" << x << "]\n\n";}
-#endif
