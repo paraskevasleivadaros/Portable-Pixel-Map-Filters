9b694a79343b77bd8fdf85f30f8347738490b707
Paraskevas Leivadaros <p3150090@aueb.gr>
2019-01-05 02:08:08 +0200

    Update main.cpp

--- neg/neg/main.cpp
+++ neg/neg/main.cpp
@@ -1,110 +1,6 @@
 //LEIVADAROS PARASKEVAS 3150090
 //KOYLOYRIS GEORGIOS 3150080
-/*
-int main(int argc, char *argv[]) {
 
-	std::string filename;
-
-	if (argc < 6) {
-
-		std::cerr << "Error: Wrong Input" << std::endl;
-		std::cerr << "====" << std::endl;
-
-	}
-	else {
-
-		filename = argv[argc - 1];
-		line();
-
-		imaging::Image *imgObj = new imaging::Image;
-
-		if (imgObj->load(filename, "ppm")) {
-
-			std::cout << "Image dimensions are: " << imgObj->getWidth() << " X " << imgObj->getHeight() << std::endl;
-			line();
-
-			int i = 1;
-			while (i < argc - 1) {
-				if (argv[i] == std::string("filter")) {
-					i++;
-					if (argv[i] == std::string("-f")) {
-						i++;
-						if (argv[i] == std::string("linear")) {
-							std::cerr << "====" << std::endl;
-							Color a, c;
-							i++;
-							a.r = (float)atof(argv[i]);
-							i++;
-							a.g = (float)atof(argv[i]);
-							i++;
-							a.b = (float)atof(argv[i]);
-							i++;
-							c.r = (float)atof(argv[i]);
-							i++;
-							c.g = (float)atof(argv[i]);
-							i++;
-							c.b = (float)atof(argv[i]);
-							i++;
-
-							FilterLinear linear(a, c);
-							*imgObj = linear << *imgObj;
-							
-
-						}
-						else if (argv[i] == std::string("gamma")) {
-
-							i++;
-							float g = (float)atof(argv[i]);
-							if (g >= 0.5 && g <= 2.0) {
-								
-								FilterGamma gamma(g);
-								*imgObj = gamma << *imgObj;
-
-							}
-							else {
-
-								line();
-								std::cerr << "Error: Gamma values must be lower than 2.0 and higher than 0.5" << std::endl;
-								line();
-
-								system("PAUSE");
-								return 1;
-							}
-						}
-						else {
-
-							line();
-							std::cerr << "Error: Wrong type of filter" << std::endl;
-							line();
-
-							system("PAUSE");
-							return 1;
-						}
-
-					}
-					else { i++; }
-
-				}
-				//else { i++; }
-			}
-		}
-		else {
-			std::cerr << "Error: Image Failed Loading\n";
-		}
-
-		//adds the name of the new image
-		std::string newfilename = "filtered_" + filename;
-
-		if (!imgObj->save(newfilename, "ppm")) { std::cerr << "Error: Image Failed Saving\n"; }
-
-		imgObj->~Image();//Destroys the pointer of image object		
-	}
-
-	system("PAUSE");
-	return 0;
-}
-}
-*/
 #include "Filter.h"
 
 void spacing() { std::cout << "- - - - - - - - - - - - - - - - - - - - - - - - -\n"; }
@@ -251,7 +147,7 @@ int main(int argc, char* argv[]) {
 		spacing();
 	}
 
-	delete imgObj; // freeing memory
+	imgObj->~Image(); // freeing memory
 
 	system("PAUSE");
 
